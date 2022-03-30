04ec0f4510de7fc9e72dc2144a5e45bf1bd9d29d
messiJod <88767533+messiJod@users.noreply.github.com>
2022-03-30 22:47:56 +0530

    Update app.js

--- app.js
+++ app.js
@@ -31,6 +31,7 @@ function randomColor() {
 
 let input = document.querySelector("input[type=textbox]");
 let div = document.querySelector("#jod");
+
 // input.addEventListener("input", (event) => {
 //   console.log(event);
 //   jod.innerText = input.value;
@@ -40,6 +41,7 @@ let div = document.querySelector("#jod");
 // using Span
 // input.addEventListener("input", (event) => {
 //   let span = document.createElement("span");
+//   console.log(event);
 //   if (event.data == "null") {
 //     div.removeChild(span);
 //   }
@@ -48,17 +50,20 @@ let div = document.querySelector("#jod");
 //   div.appendChild(span);
 // });
 
-// With keydown
-// input.addEventListener("keydown", (event) => {
-//   let span = document.createElement("span");
-//   if (event.key != "Backspace") {
-//     span.textContent = event.key;
-//     span.style.color = randomColor();
-//     div.appendChild(span);
-//   } else {
-//     console.log(div.lastChild);
-//   }
-// });
+// With input
+input.addEventListener("input", (event) => {
+  let span = document.createElement("span");
+  if (event.inputType != "deleteContentBackward") {
+    span.textContent = event.data;
+    span.style.color = randomColor();
+    div.appendChild(span);
+  } else {
+    // let allSpan = document.querySelector("#jod span:last-child");
+    // let spans = document.querySelectorAll("span");
+    // console.log(spans[spans.length - 1]);
+    div.removeChild(div.lastChild);
+  }
+});
 
 // let wrapper = document.createElement("ul");
 // let child = document.createElement("li");
@@ -75,10 +80,10 @@ let div = document.querySelector("#jod");
 // }
 // chnge();
 
-let strop = "jaddu";
-let array = [];
-for (let i = 0; i < strop.length; i++) {
-  array.push(strop[i]);
-  array[i]
-}
-console.log(array);
+// let strop = "jaddu";
+// let array = [];
+// for (let i = 0; i < strop.length; i++) {
+//   array.push(strop[i]);
+//   array[i]
+// }
+// console.log(array);
